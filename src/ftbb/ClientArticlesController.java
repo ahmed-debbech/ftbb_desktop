771483221d0302a77242f362c5e04f3714a6a3c8
ahmed-debbech <debbech.ahmed@gmail.com>
2021-03-08 17:24:42 +0100

    design the ui of the article grid

---
+++ src/ftbb/ClientArticlesController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ftbb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed
+ */
+public class ClientArticlesController implements Initializable {
+
+    @FXML
+    private ScrollPane scroller;
+    @FXML
+    private GridPane article_grid;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/ftbb/PostsController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ftbb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed
+ */
+public class PostsController implements Initializable {
+
+    @FXML
+    private ImageView image;
+    @FXML
+    private Text title;
+    @FXML
+    private HBox likes;
+    @FXML
+    private Label like_number;
+    @FXML
+    private HBox comments;
+    @FXML
+    private Label com_number;
+    @FXML
+    private Label date;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
