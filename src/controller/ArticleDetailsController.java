06e2bb41f637f8f56ee78072bbd5b3015ef05855
ahmed-debbech <debbech.ahmed@gmail.com>
2021-03-26 22:35:09 +0100

    refactor to another packages

---
+++ src/controller/ArticleDetailsController.java
@@ -0,0 +1,273 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import Enitity.Article;
+import Enitity.Comment;
+import Enitity.Like;
+import Service.ServiceArticle;
+import Service.ServiceComment;
+import Service.ServiceLikes;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javax.imageio.ImageIO;
+import utils.Passable;
+import utils.Utilities;
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed
+ */
+public class ArticleDetailsController implements Initializable {
+    public static String CLIENT_ID = "122";
+    
+    private Article ref;
+    @FXML
+    private ImageView image;
+    @FXML
+    private Label title;
+    @FXML
+    private Text text;
+    @FXML
+    private HBox likes;
+    @FXML
+    private ImageView like_icon;
+    @FXML
+    private Label like_number;
+    @FXML
+    private HBox comments;
+    @FXML
+    private Label com_number;
+    @FXML
+    private Label author;
+    @FXML
+    private Label date;
+    @FXML
+    private ScrollPane scroller;
+    @FXML
+    private VBox comlist;
+    @FXML
+    private TextField comment_content;
+    @FXML
+    private ComboBox<String> filter;
+    @FXML
+    private Button sharefb;
+    @FXML
+    private Button sharetw;
+    /**
+     * Initializes the controller class.
+     */
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        ServiceArticle sa = new ServiceArticle();
+        int id= Passable.getInstance().getNumberData();
+        Passable.getInstance().erase();
+         this.ref = sa.getArticle(id);
+         System.out.println("ldld" + this.ref.getArticle_id());
+           this.title.setText(ref.getTitle());
+           this.date.setText(Utilities.timestampConverter(ref.getDate()));
+           this.text.setText(ref.getText());
+           this.filter.getItems().add("None");
+        this.filter.getItems().add("New");
+        this.filter.getItems().add("Hot");
+
+        Image im = null;
+        try {
+            URL imageUrl = new URL(ref.getPhoto_url());
+            InputStream in = imageUrl.openStream();
+            BufferedImage image = ImageIO.read(in);
+            im = SwingFXUtils.toFXImage(image, null);
+            in.close();
+        }catch (IOException ioe) {
+           im = new Image("resources/default-article.jpg");
+        }
+        this.image.setImage(im);
+        
+        ServiceLikes sl = new ServiceLikes();
+        if(sl.getLike(ref.getArticle_id(), -1, Integer.parseInt(CLIENT_ID)) == true){
+            this.like_icon.setImage(new Image("resources/like-press.png"));
+        }else{
+           this.like_icon.setImage(new Image("resources/like.png"));
+        }
+        int nm = sl.countLikes(ref.getArticle_id(), -1);
+        this.like_number.setText(String.valueOf(nm));
+        this.author.setText(ref.getAuthor());
+        ServiceComment ser = new ServiceComment();
+        int nm_cm = ser.countComments(String.valueOf(ref.getArticle_id()));
+        this.com_number.setText(String.valueOf(nm_cm));
+        //**** load comments
+        loadComments();
+    }    
+    private void loadComments(){
+        this.comlist.getChildren().clear();
+        ServiceComment sc = new ServiceComment();
+           List<Comment> list =  sc.showComment(String.valueOf(ref.getArticle_id()));
+            try{
+                int pos = 0;
+                for(Comment a : list){
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("FXMLCommentClientView.fxml"));
+                    Node postbox = loader.load();
+                    CommentClientViewController pc = loader.getController();
+                    pc.setData(a, pos, this.comlist);
+                    this.comlist.getChildren().add(postbox);
+                    pos++;
+                }
+            }catch(IOException e){
+                  System.out.println("error");
+            }
+    }
+    private void loadComments(List<Comment> li){
+        this.comlist.getChildren().clear();
+        ServiceComment sc = new ServiceComment();
+           List<Comment> list =  li;
+            try{
+                int pos = 0;
+                for(Comment a : list){
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("FXMLCommentClientView.fxml"));
+                    Node postbox = loader.load();
+                    CommentClientViewController pc = loader.getController();
+                    pc.setData(a, pos, this.comlist);
+                    this.comlist.getChildren().add(postbox);
+                    pos++;
+                }
+            }catch(IOException e){
+                  System.out.println("error");
+            }
+    }
+    @FXML
+    private void passDetails(MouseEvent event) {
+    }
+
+    @FXML
+    private void onClickLike(MouseEvent event) {
+         ServiceLikes sl = new ServiceLikes();
+        Like l = new Like(ref.getArticle_id(), -1,Integer.parseInt(CLIENT_ID));
+        if(!sl.isExisted(l)){
+            // add the like
+            sl.add(l);
+            this.like_icon.setImage(new Image("resources/like-press.png"));
+            int x= Integer.parseInt(this.like_number.getText());
+            x++;
+            this.like_number.setText(String.valueOf(x));
+        }else{
+            //else remove it
+            sl.remove(l);
+            this.like_icon.setImage(new Image("resources/like.png"));
+            int x= Integer.parseInt(this.like_number.getText());
+            x--;
+            this.like_number.setText(String.valueOf(x));
+        }
+    }
+
+    @FXML
+    private void comment(ActionEvent event) {
+        String comment = this.comment_content.getText();
+        ServiceComment sc = new ServiceComment();
+        Comment a = new Comment();
+        a.setClient_id(Integer.parseInt(CLIENT_ID));
+        a.setArticle_id(this.ref.getArticle_id());
+        a.setContent(comment);
+        a.setId(Utilities.generatedId("comment", "id"));
+        sc.addComment(a);
+        ServiceArticle ss = new ServiceArticle();
+        if(this.filter.getSelectionModel().getSelectedIndex() == 1){
+            System.out.println("first is selected");
+            List<Comment> list = ss.sortByNew(ref.getArticle_id());
+            loadComments(list);
+        }else{  
+            if(this.filter.getSelectionModel().getSelectedIndex() == 2){
+                System.out.println("second is selected");
+                List<Comment> list = ss.sortByHot(ref.getArticle_id());
+                loadComments(list);
+            }else{
+                loadComments();
+            }
+        }
+        this.comment_content.setText("");
+    }
+
+    @FXML
+    private void itemChanged(ActionEvent event) {
+        ServiceArticle sc = new ServiceArticle();
+        
+        if(this.filter.getSelectionModel().getSelectedIndex() == 1){
+            System.out.println("first is selected");
+            List<Comment> list = sc.sortByNew(ref.getArticle_id());
+            loadComments(list);
+        }else{  
+            if(this.filter.getSelectionModel().getSelectedIndex() == 2){
+                System.out.println("second is selected");
+                List<Comment> list = sc.sortByHot(ref.getArticle_id());
+                loadComments(list);
+            }
+        }
+    }
+
+    @FXML
+    private void shareFacebook(ActionEvent event) {
+         try {
+             String u  = "https://www.facebook.com/dialog/share?" +
+"app_id=1155678251616612" +
+"&display=popup" +
+                     "&quote=" +URLEncoder.encode(this.title.getText(),  StandardCharsets.UTF_8.toString())+ 
+"&href=https%3A%2F%2Fwww.ftbb.org.tn%2F";
+            URI uri= new URI(u);
+
+            java.awt.Desktop.getDesktop().browse(uri);
+             System.out.println("Web page opened in browser");
+
+       } catch (Exception e) {
+
+        e.printStackTrace();
+       }
+    }
+
+    @FXML
+    private void shareTwitter(ActionEvent event) {
+         try {
+
+             String k = "http://twitter.com/share?text="+URLEncoder.encode(this.title.getText(),  StandardCharsets.UTF_8.toString())+"%0A&url=https%3A%2F%2Fwww.ftbb.org.tn%2F";
+            URI uri= new URI(k);
+
+            java.awt.Desktop.getDesktop().browse(uri);
+             System.out.println("Web page opened in browser");
+
+       } catch (Exception e) {
+
+        e.printStackTrace();
+       }
+    }
+    
+}
---
+++ src/controller/ArticleEditorController.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import Enitity.Article;
+import Service.ServiceArticle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import utils.Passable;
+import utils.Utilities;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed
+ */
+public class ArticleEditorController implements Initializable {
+
+    @FXML
+    private Button add_but;
+    @FXML
+    private TextArea text;
+    @FXML
+    private TextField title;
+    @FXML
+    private TextField author;
+    @FXML
+    private ComboBox<String> category;
+    @FXML
+    private TextField photo_dir;
+    @FXML
+    private Button browse_photo;
+    @FXML
+    private Label article_id;
+    
+    private int mode; // 0 for add , 1 for modify
+    private Article whatToMod;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.whatToMod = (Article) Passable.getInstance().getAnyData();
+        this.mode = Passable.getInstance().getNumberData();
+        System.out.println("mode ==" + Passable.getInstance().getNumberData());
+        Passable.getInstance().erase();
+        category.getItems().add(Article.toStringCatTypes(Article.getBREAKING_NEWS()));
+        category.getItems().add(Article.toStringCatTypes(Article.getHOT()));
+        category.getItems().add(Article.toStringCatTypes(Article.getANNOUNCE()));
+        category.getItems().add(Article.toStringCatTypes(Article.getMISC()));
+        if(this.mode == 0){
+            this.add_but.setText("Add New");
+        }else{
+            this.add_but.setText("Change");
+            if(whatToMod == null){
+                    Utilities.alert("WARNING!", "Please select an article from the list!");
+                }else{
+                    article_id.setText(String.valueOf(whatToMod.getArticle_id()));
+                    text.setText(whatToMod.getText());
+                    title.setText(whatToMod.getTitle());
+                    author.setText(whatToMod.getAuthor());
+                    photo_dir.setText(whatToMod.getPhoto_url());
+                    switch(whatToMod.getCategory()){
+                        case 0: 
+                            category.setValue(Article.toStringCatTypes(whatToMod.getCategory()));
+                            break;
+                        case 1:
+                            category.setValue(Article.toStringCatTypes(whatToMod.getCategory()));
+                            break;
+                        case 2:
+                            category.setValue(Article.toStringCatTypes(whatToMod.getCategory()));
+                            break;
+                        case 3:
+                            category.setValue(Article.toStringCatTypes(whatToMod.getCategory()));
+                            break;
+                    }
+                }
+        }
+    }    
+
+
+    private boolean controlFields(){
+        if(this.title.getText().equals("") || 
+                this.text.getText().equals("") || 
+                this.author.getText().equals("") || 
+                this.photo_dir.getText().equals("") ||
+                this.category.getSelectionModel().getSelectedItem() .equals("Select Category")){
+            Utilities.alert("Error", "All field are mendatory!");
+            return false;
+        }
+        return true;
+    }
+    @FXML
+    private void modArticle(ActionEvent event) {
+        if(!controlFields()){
+            return;
+        }
+        if(this.mode == 0){
+              ServiceArticle sp = new ServiceArticle();
+              String dir = Utilities.pathToUrl(photo_dir.getText());
+                Article a = new Article(title.getText(), text.getText(), author.getText(), dir, category.getSelectionModel().getSelectedItem());
+                sp.addArticle(a);
+        }else{
+            ServiceArticle sa = new ServiceArticle();
+            String dir = Utilities.pathToUrl(photo_dir.getText());
+            Article a = new Article(Integer.parseInt(article_id.getText()), title.getText() , text.getText(), author.getText(), dir, category.getSelectionModel().getSelectedItem());
+            sa.modArticle(a);
+        }
+        Stage stage = (Stage) add_but.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void browseImage(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Pick a banner file!");
+        fileChooser.setInitialDirectory(new File("C:\\xampp\\htdocs\\uploads"));
+        Stage stage = new Stage();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+            );
+        File file = fileChooser.showOpenDialog(stage);
+        try {
+                BufferedImage bufferedImage = ImageIO.read(file);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                photo_dir.setText(file.getAbsolutePath());
+            } catch (IOException ex) {
+                System.out.println("could not get the image");
+            }
+    }
+    
+    
+}
