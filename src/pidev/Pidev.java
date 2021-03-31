1f9594f3538e583cc1f8d7fb2b942dad71c6ddc9
ahmed-debbech <debbech.ahmed@gmail.com>
2021-03-31 11:33:42 +0100

    add template design

--- src/pidev/Pidev.java
+++ src/pidev/Pidev.java
@@ -9,20 +9,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import static javafx.scene.paint.Color.color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
- * @author Ahmed
+ * @author PC
  */
 public class Pidev extends Application {
-    
+    public static Boolean isSplashLoader = false;
+    double x,y = 0;
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/news/src/view/FXMLArticlesAdminView.fxml"));
+      Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        //Scene scene = new Scene(root);
         
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
         Scene scene = new Scene(root);
-        
+        //set transparent
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
