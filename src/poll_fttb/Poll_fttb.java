2d3791e60560e9bd3517f173ae15cbf0794b0619
slimjaaf <slim.jaafoura@esprit.tn>
2021-03-31 22:28:24 +0100

    fix evry thing

--- src/poll_fttb/Poll_fttb.java
+++ src/poll_fttb/Poll_fttb.java
@@ -19,7 +19,7 @@ public class Poll_fttb extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("PollCientView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("PollAdminView.fxml"));
         
         Scene scene = new Scene(root);
         
