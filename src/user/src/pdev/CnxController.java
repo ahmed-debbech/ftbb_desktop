dd0b73ccba5125cf939007023435db73a28b9dac
ahmed-debbech <debbech.ahmed@gmail.com>
2021-03-31 13:45:09 +0100

    add working client profile button

--- src/user/src/pdev/CnxController.java
+++ src/user/src/pdev/CnxController.java
@@ -8,6 +8,8 @@ package user.src.pdev;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -38,9 +40,11 @@ import pidev.LoginController;
 import static pidev.Pidev.isSplashLoader;
 import user.src.Entities.Admin;
 import user.src.Entities.Client;
+import user.src.Entities.Password;
 import user.src.Service.ServiceAdmin;
 import user.src.Service.ServiceClient;
 import user.src.Service.ServiceCnx;
+import user.src.Utils.Utilities;
 
 /**
  *
@@ -164,18 +168,81 @@ public class CnxController implements Initializable {
 
     @FXML
      private void Inscription(ActionEvent event) throws IOException {
-         Blogin.getScene().getWindow().hide();
+      
+           Client cl = new Client();
+             boolean a=Utilities.validationEmail(tfemail1.getText());        
+        if (a=false) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("WARNING!");
+            alert.setHeaderText(null);
+            alert.setContentText("votre email n'est pas valide");
+            Optional<ButtonType> result = alert.showAndWait();
+        }  else if (tfnom.getText().isEmpty()) {
+             Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("WARNING!");
+            alert.setHeaderText(null);
+            alert.setContentText("votre nom n'est pas valide");
+            Optional<ButtonType> result = alert.showAndWait();  
+        }   else if (tfprenom.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("WARNING!");
+            alert.setHeaderText(null);
+            alert.setContentText("votre prenom n'est pas valide");
+            Optional<ButtonType> result = alert.showAndWait(); 
+            
+        }   else if (tfpass.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("WARNING!");
+            alert.setHeaderText(null);
+            alert.setContentText("votre password n'est pas valide");
+            Optional<ButtonType> result = alert.showAndWait(); 
+        } else { if ((!Rmale.isSelected())&&(!Rfemme.isSelected())) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("WARNING!");
+            alert.setHeaderText(null);
+            alert.setContentText("Selectionner le sexe ");
+            Optional<ButtonType> result = alert.showAndWait(); }
+        else {
+        ServiceClient sc = new ServiceClient();
+        ServiceAdmin sa = new ServiceAdmin();
+        Password pass =new Password ();
+        pass.setPwd(tfpass.getText());
+        sa.AddPasswordAdmin(pass);
+        cl.setPassword_id(pass.getPassword_id());
         
-     
-        Parent root = FXMLLoader.load(getClass().getResource("/user/src/pdev/InscriptionClient.fxml"));
+        cl.setName(tfnom.getText());
+        cl.setSurname(tfprenom.getText());
+        cl.setEmail(tfemail1.getText());
         
-        Scene scene = new Scene(root);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-        stage.show();
-        stage.setResizable(false);
-    }
+        LocalDate d = tdate.getValue();
+        System.out.println(d);
+       cl.setBirthday(Date.valueOf(d));//**********
+        int nb = Integer.parseInt(tfnumber.getText());
+        cl.setNumber(nb);
+        if (Rmale.isSelected()) {
 
+            cl.setSex("Male");
+
+        } else {
+            if (Rfemme.isSelected()) {
+                cl.setSex("Femme");
+            }
+
+        }
+        
+       
+        System.out.println(cl.toString());
+        sc.AddClient(cl);
+         
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Succes!");
+            alert.setHeaderText(null);
+            alert.setContentText("Inscription validée");
+            Optional<ButtonType> result = alert.showAndWait(); 
+    }}}
+    
+
+    @FXML
     private void ResetPassword(ActionEvent event) throws IOException {
      Blogin.getScene().getWindow().hide();
         
@@ -244,4 +311,5 @@ public class CnxController implements Initializable {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
 }
